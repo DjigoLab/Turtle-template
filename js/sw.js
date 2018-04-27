fa6f05c00dc0cb517ce06cfc18d322f52f636e96
Antonio-Djigo <antonio@secret-source.eu>
2018-04-27 14:11:31 +0100

    fixeso

--- js/sw.js
+++ js/sw.js
@@ -1,5 +1,14 @@
 //This is the "Offline page" service worker
-
+if (navigator.serviceWorker.controller) {
+    console.log('[PWA Builder] active service worker found, no need to register')
+} else {
+    //Register the ServiceWorker
+    navigator.serviceWorker.register('sw.js', {
+        scope: './'
+    }).then(function (reg) {
+        console.log('Service worker has been registered for scope:' + reg.scope);
+    });
+}
 //Install stage sets up the offline page in the cahche and opens a new cache
 self.addEventListener('install', function(event) {
     var offlinePage = new Request('index.html');
